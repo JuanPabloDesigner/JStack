7d8d37719adba0334a3e730e7671e5adfcb7bd11
JuanPabloDesigner <jaypy.uxdesign@gmail.com>
2023-04-25 11:19:12 -0300

    forgot to commit :sweat_smile:

---
+++ firstapi/src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Mateus',
+    },
+    {
+        id: 2,
+        name: 'Juan',
+    },
+    {
+        id: 3,
+        name: 'João',
+    },
+];
